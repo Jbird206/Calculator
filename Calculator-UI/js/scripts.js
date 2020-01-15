a5e6473728d10a633da381d7281d158d03fc52af
Jason M <jbird206@gmail.com>
2020-01-15 15:52:43 -0800

    Add files via upload

---
+++ Calculator-UI/js/scripts.js
@@ -0,0 +1,30 @@
+// Business(back-end) logic ///////////////////////////////
+
+//declare the variable as add, make it a function. return with adding the given parameters.(number1,number2)
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+// user interface (front-end) logic:
+//gathers user input values,parses them into intergers and calling our add() function when user submits the form with the button.
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
